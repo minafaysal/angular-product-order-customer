9d16580b54616aa0f4f4bc2a549f3004fb9207d5
minafaysal <minafaysal.94@gmail.com>
2024-05-10 00:24:36 +0300

    add shared module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-product-order-customer' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-product-order-customer');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-product-order-customer');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, products-dashboard');
   });
 });
