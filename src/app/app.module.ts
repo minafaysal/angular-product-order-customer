c28a90b6d75215f69cb774408cf3c4e9dbfad2f2
minafaysal <minafaysal.94@gmail.com>
2024-05-10 02:22:54 +0300

    add main pages and routes config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductsComponent } from './pages/products/products.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { OrderDetailsComponent } from './pages/order-details/order-details.component';
 
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, ProductsComponent, OrdersComponent, OrderDetailsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from './components/not-found/not-found/not-found.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotFoundComponent
+  ],
   imports: [CommonModule],
 })
 export class SharedModule {}
