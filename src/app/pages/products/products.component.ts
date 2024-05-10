b8178ee56d1202c50f49b6667f8dc8dd32090a79
minafaysal <minafaysal.94@gmail.com>
2024-05-10 20:57:27 +0300

    get products

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../shared/models/product.model';
+import { ProductService } from '../../shared/services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    // Call method to fetch products when component initializes
+    this.getProducts();
+  }
+  // Method to fetch products from ProductService
+  getProducts(): void {
+    this.productService
+      .getProducts()
+      .subscribe((products) => (this.products = products));
+  }
 }
