68630ac2857e878c8161fc1970fdf0ea59176a06
minafaysal <minafaysal.94@gmail.com>
2024-05-10 21:26:11 +0300

    optimize product page performance

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product.model';
 import { ProductService } from '../../shared/services/product.service';
+import { ComponentBase } from '../../shared/base/common.base';
+import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent extends ComponentBase implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService) {
+      super();
+  }
 
   ngOnInit(): void {
     // Call method to fetch products when component initializes
@@ -18,8 +22,16 @@ export class ProductsComponent implements OnInit {
   }
   // Method to fetch products from ProductService
   getProducts(): void {
-    this.productService
-      .getProducts()
-      .subscribe((products) => (this.products = products));
+     this.productService
+       .getProducts()
+       .pipe(takeUntil(this.destroy$)) 
+       .subscribe((products) => {
+         this.products = products;
+       });
+  }
+
+  // TrackBy function to improve rendering performance
+  trackByProductId(index: number, product: Product): number {
+    return product.ProductId;
   }
 }
